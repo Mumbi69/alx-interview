8fe0f4210516e85760a6f3b9add5d96c7bfa6a0d
Mumbi69 <winfriendmumbi@gmail.com>
2024-01-24 16:49:58 +0300

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 """Log parsing"""
+
+
 from sys import stdin
 
-status_code_counts = {
+
+status_counts = {
     200: 0,
     301: 0,
     400: 0,
@@ -20,7 +23,7 @@ count = 0
 def print_all_statistics():
     """Prints total file size and counts of each status code."""
     print("File size:", total_file_size)
-    for key, value in status_code_counts.items():
+    for key, value in status_counts.items():
         if value:
             print("{}: {}".format(key, value))
 
@@ -30,13 +33,13 @@ try:
         count += 1
 
         try:
-            line_parts = line.split(" ")
-            status_code = int(line_parts[7])
-            file_size = int(line_parts[8])
-        except (IndexError, ValueError):
+            line = line.split(" ")
+            status_code = int(line[7])
+            file_size = int(line[8])
+        except (IndexError, TypeError):
             continue
 
-        status_code_counts[status_code] += 1
+        status_counts[status_code] += 1
         total_file_size += file_size
 
         if count == 10:
