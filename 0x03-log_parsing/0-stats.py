d7a3aaa8e53b6400fb73175b5e8a500a4072dc2c
Mumbi69 <winfriendmumbi@gmail.com>
2024-01-24 17:00:35 +0300

    logs

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,14 +3,13 @@
 
 from sys import stdin
 
-
 status_dict = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 total_file_size = 0
 count = 0
 
 
 def print_all_statistics():
-    """Prints total file size and counts of each status code"""
+    """Prints total file size and counts of each status code."""
     print("File size:", total_file_size)
     for key, value in status_dict.items():
         if value:
@@ -20,17 +19,22 @@ def print_all_statistics():
 try:
     for line in stdin:
         count += 1
+
         try:
-            line = line.split(" ")
-            status_code = int(line[7])
-            file_size = int(line[8])
-        except (IndexError, TypeError):
+            line_parts = line.split(" ")
+            status_code = int(line_parts[7])
+            file_size = int(line_parts[8])
+        except (IndexError, ValueError):
             continue
+
         status_dict[status_code] += 1
         total_file_size += file_size
+
         if count == 10:
             print_all_statistics()
             count = 0
+
     print_all_statistics()
+
 except KeyboardInterrupt:
     print_all_statistics()
