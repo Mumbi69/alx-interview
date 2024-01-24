3afcf4c12f65f4c175b6cbd33dde44818571fa04
Mumbi69 <winfriendmumbi@gmail.com>
2024-01-24 16:44:10 +0300

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 """Log parsing"""
-import sys
-import signal
-
+from sys import stdin
 
 status_code_counts = {
     200: 0,
@@ -16,42 +14,36 @@ status_code_counts = {
 }
 
 total_file_size = 0
-lines_processed = 0
+count = 0
 
 
-def print_statistics():
-    """represents print statistics"""
-    print(f"File size: {total_file_size}")
-    for code, count in sorted(status_code_counts.items()):
-        if count > 0:
-            print(f"{code}: {count}")
+def print_all_statistics():
+    """Prints total file size and counts of each status code."""
+    print("File size:", total_file_size)
+    for key, value in status_code_counts.items():
+        if value:
+            print("{}: {}".format(key, value))
 
-
-def signal_handler(sig, frame):
-    """represents signal handler"""
-    print_statistics()
-    sys.exit(0)
-
-
-signal.signal(signal.SIGINT, signal_handler)
 
 try:
-    for line in sys.stdin:
-        parts = line.split()
+    for line in stdin:
+        count += 1
 
-        if len(parts) == 10 and parts[5].isdigit() and parts[8].isdigit():
-            status_code = int(parts[8])
-            file_size = int(parts[9])
+        try:
+            line_parts = line.split(" ")
+            status_code = int(line_parts[7])
+            file_size = int(line_parts[8])
+        except (IndexError, ValueError):
+            continue
 
-            total_file_size += file_size
+        status_code_counts[status_code] += 1
+        total_file_size += file_size
 
-            status_code_counts[status_code] += 1
+        if count == 10:
+            print_all_statistics()
+            count = 0
 
-            lines_processed += 1
-
-            if lines_processed % 10 == 0:
-                print_statistics()
+    print_all_statistics()
 
 except KeyboardInterrupt:
-    print_statistics()
-    sys.exit(0)
+    print_all_statistics()
