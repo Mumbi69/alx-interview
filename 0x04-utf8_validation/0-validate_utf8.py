9685339a341850286deff5637de55781eaab9cf5
Mumbi69 <winfriendmumbi@gmail.com>
2024-01-31 02:14:34 +0300

    validate_utf8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -7,21 +7,21 @@ from typing import List
 def validUTF8(data: List[int]) -> bool:
     """Representation of validUTF8 function"""
     count = 0
-
-    for byte in data:
-        binary_representation = format(byte, '08b')
-
-        if binary_representation.startswith("0") and count == 0:
+    for s in data:
+        binary = bin(s)[2:].zfill(8)
+        if binary.startswith("0") and count == 0:
             continue
-        elif binary_representation.startswith("110") and count == 0:
+        if binary.startswith("110") and count == 0:
             count = 1
-        elif binary_representation.startswith("1110") and count == 0:
+            continue
+        if binary.startswith("1110") and count == 0:
             count = 2
-        elif binary_representation.startswith("11110") and count == 0:
+            continue
+        if binary.startswith("11110") and count == 0:
             count = 3
-        elif binary_representation.startswith("10") and count > 0:
+            continue
+        if binary.startswith("10") and count > 0:
             count -= 1
-        else:
-            return False
-
+            continue
+        return False
     return count == 0
