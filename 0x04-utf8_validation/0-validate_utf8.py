515fc48643d3f7ea4e29583a0bae76329a500d89
Mumbi69 <winfriendmumbi@gmail.com>
2024-01-31 02:25:20 +0300

    validate_utf8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,27 +1,46 @@
 #!/usr/bin/python3
 """The function validUTF8 module"""
 
-from typing import List
 
+def validUTF8(data):
+    """
+    Determines if a given data set represents a valid UTF-8 encoding.
 
-def validUTF8(data: List[int]) -> bool:
-    """Representation of validUTF8 function"""
-    count = 0
-    for s in data:
-        binary = bin(s)[2:].zfill(8)
-        if binary.startswith("0") and count == 0:
-            continue
-        if binary.startswith("110") and count == 0:
-            count = 1
-            continue
-        if binary.startswith("1110") and count == 0:
-            count = 2
-            continue
-        if binary.startswith("11110") and count == 0:
-            count = 3
-            continue
-        if binary.startswith("10") and count > 0:
-            count -= 1
-            continue
-        return False
-    return count == 0
+    :param data: List of integers representing 1 byte of data each.
+    :return: True if data is a valid UTF-8 encoding, else return False.
+    """
+    def is_start_byte(byte):
+        return (byte & 0b10000000) == 0b00000000
+
+    def is_follow_byte(byte):
+        return (byte & 0b11000000) == 0b10000000
+
+    i = 0
+    while i < len(data):
+        if (data[i] & 0b10000000) == 0b00000000:
+            i += 1
+        elif (data[i] & 0b11100000) == 0b11000000:
+            if i + 1 < len(data)\
+                    and is_follow_byte(data[i + 1]):
+                i += 2
+            else:
+                return False
+        elif (data[i] & 0b11110000) == 0b11100000:
+            if i + 2 < len(data)\
+                    and is_follow_byte(data[i + 1])\
+                    and is_follow_byte(data[i + 2]):
+                i += 3
+            else:
+                return False
+        elif (data[i] & 0b11111000) == 0b11110000:
+            if i + 3 < len(data)\
+                    and is_follow_byte(data[i + 1])\
+                    and is_follow_byte(data[i + 2])\
+                    and is_follow_byte(data[i + 3]):
+                i += 4
+            else:
+                return False
+        else:
+            return False
+
+    return True
