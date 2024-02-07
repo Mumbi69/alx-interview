4f56401368d57f6815fc19b85781e0a88077e0c7
Mumbi69 <winfriendmumbi@gmail.com>
2024-02-07 14:39:48 +0300

    N Queens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,34 +1,36 @@
 #!/usr/bin/python3
-"""module for N Queens"""
+"""
+The N queens puzzle is the challenge of placing N non-attacking queens
+on an NxN chessboard. Write a program that solves the N queens problem.
+"""
 
 import sys
-
 
 if len(sys.argv) != 2:
     print("Usage: nqueens N")
     sys.exit(1)
 
 try:
-    size = int(sys.argv[1])
+    N = int(sys.argv[1])
 except ValueError:
-    print("size must be a number")
+    print("N must be a number")
     sys.exit(1)
 
-if size < 4:
-    print("size must be at least 4")
+if N < 4:
+    print("N must be at least 4")
     sys.exit(1)
 
-chessboard = []
-for i in range(size):
+board = []
+for i in range(N):
     row = []
-    for j in range(size):
+    for y in range(N):
         row.append(0)
-    chessboard.append(row)
-queens_positions = []
+    board.append(row)
+queens = []
 
 
-def mark_available_spots(queens, board):
-    """This function checks and marks available spots on a chessboard."""
+def available_spots(queens, board):
+    """Check and mark available spots on a chessboard."""
     for i, row in enumerate(board):
         for j, square in enumerate(row):
             for x, y in queens:
@@ -36,22 +38,25 @@ def mark_available_spots(queens, board):
                     board[i][j] = 1
 
 
-def place_queens(size, queens, board, solutions):
-    """This function places queens on a chessboard of given size"""
-    if len(queens) == size:
+def queen_down(N, queens, board, final_combo):
+    """
+    Place queens on a chessboard of size N,
+    exploring all combinations and returning unique solutions.
+    """
+    if len(queens) == N:
         sorted_queens = sorted(queens, key=lambda x: x[0])
-        if sorted_queens not in solutions:
-            solutions.append(queens)
-        return solutions
-    mark_available_spots(queens, board)
+        if sorted_queens not in final_combo:
+            final_combo.append(queens)
+        return final_combo
+    available_spots(queens, board)
     for i, row in enumerate(board):
         for j, square in enumerate(row):
             if not square:
-                board_copy = [r[:] for r in board]
-                place_queens(size, queens + [[i, j]], board_copy, solutions)
-    return solutions
+                board_cpy = [r[:] for r in board]
+                queen_down(N, queens + [[i, j]], board_cpy, final_combo)
+    return final_combo
 
 
-queens_positions = place_queens(size, queens_positions, chessboard, [])
-for queen in queens_positions:
+queens = queen_down(N, queens, board, [])
+for queen in queens:
     print(queen)
