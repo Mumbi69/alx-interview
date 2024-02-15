588ead751417a896a0caa3cc1672bc2702406da3
Mumbi69 <winfriendmumbi69@gmail.com>
2024-02-15 15:14:31 +0300

    Star Wars Characters

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+
+const request = require('request')
+
+if (process.argv.length !== 3) {
+    console.error('Usage: node 0-starwars_characters.js <movie_id>')
+    process.exit(1)
+}
+
+const movieId = process.argv[2]
+
+request(`https: // swapi.dev / api / films /${movieId} /`, (error, response, body)= > {
+    if (error) {
+        console.error('Error:', error)
+        process.exit(1)
+    }
+
+    const film=JSON.parse(body)
+
+    film.characters.forEach((characterUrl)=> {
+        request(characterUrl, (error, response, body)= > {
+            if (error) {
+                console.error('Error:', error)
+                process.exit(1)
+            }
+
+            const character=JSON.parse(body)
+            console.log(character.name)
+        })
+    })
+})
