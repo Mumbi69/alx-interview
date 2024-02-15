3055bd56c8c6c560c03356ef5dbdedc6e8de0ca3
Mumbi69 <winfriendmumbi69@gmail.com>
2024-02-15 15:19:37 +0300

    Star Wars Characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,30 +2,20 @@
 
 const request = require('request');
 
-if (process.argv.length !== 3) {
-    console.error('Usage: node 0-starwars_characters.js <movie_id>')
-    process.exit(1)
-}
-
-const movieId = process.argv[2]
-
-request(`https: // swapi.dev / api / films /${movieId} /`, (error, response, body)= > {
-    if (error) {
-        console.error('Error:', error)
-        process.exit(1)
+request(`https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`, async (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    const chars = JSON.parse(body).characters;
+    for (const character of chars) {
+      const p = new Promise((resolve, reject) => {
+        request(character, function (error, response, body) {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(JSON.parse(body).name);
+          }
+        });
+      });
+      console.log(await p);
     }
-
-    const film=JSON.parse(body)
-
-    film.characters.forEach((characterUrl)=> {
-        request(characterUrl, (error, response, body)= > {
-            if (error) {
-                console.error('Error:', error)
-                process.exit(1)
-            }
-
-            const character=JSON.parse(body);
-            console.log(character.name);
-        })
-    })
-})
+  }
+});
