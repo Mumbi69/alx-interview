f3746acdc70bb90ba08183ab1d838150eaa24043
Mumbi69 <winfriendmumbi69@gmail.com>
2024-03-13 12:44:29 +0300

    prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -4,36 +4,38 @@
 
 def is_prime(num):
     """check if a number is prime"""
-    if num < 2:
+    if (num <= 1):
         return False
-    for i in range(2, int(num ** 0.5) + 1):
-        if num % i == 0:
+    i = 2
+    while (i * i) <= num:
+        if (num % i) == 0:
             return False
+        i += 1
     return True
 
 
-def get_primes_up_to_n(n):
+def primes_up_to_n(n):
     """generate a list of prime numbers up to a given n"""
-    return [i for i in range(2, n+1) if is_prime(i)]
+    primes_up_to_n = 0
+    for i in range(2, n + 1):
+        if is_prime(i):
+            primes_up_to_n += 1
+    return primes_up_to_n
 
 
 def isWinner(x, nums):
     """determines the winner based on whether the number
     of primes is even or odd"""
-    wins = {'Maria': 0, 'Ben': 0}
+    ben_w = maria_w = 0
 
-    for n in nums:
-        primes = get_primes_up_to_n(n)
-        num_primes = len(primes)
-
-        if num_primes % 2 == 0:
-            wins['Ben'] += 1
-        else:
-            wins['Maria'] += 1
-
-    if wins['Maria'] > wins['Ben']:
-        return 'Maria'
-    elif wins['Maria'] < wins['Ben']:
-        return 'Ben'
-    else:
+    if x <= 0 or nums is None or len(nums) == 0:
         return None
+
+    for i in range(x):
+        if primes_up_to_n(nums[i]) % 2 == 0:
+            ben_w += 1
+        else:
+            maria_w += 1
+    if ben_w == maria_w:
+        return None
+    return 'Ben' if ben_w > maria_w else 'Maria'
